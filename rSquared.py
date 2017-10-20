43f9a96479358847ab63150decd88ec67a4d23c7
Christopher Whetsel <cjwgr5@mail.missouri.edu>
2017-10-20 10:59:27 -0500

    Adding files from lesson 5

---
+++ rSquared.py
@@ -0,0 +1,19 @@
+import numpy as np
+#r squared tells us how good our model is. closer to 0 is bad. closer to 1 is good 
+def compute_r_squared(data, predictions):
+    # Write a function that, given two input numpy arrays, 'data', and 'predictions,'
+    # returns the coefficient of determination, R^2, for the model that produced 
+    # predictions.
+    # 
+    # Numpy has a couple of functions -- np.mean() and np.sum() --
+    # that you might find useful, but you don't have to use them.
+    #datas = y
+    #predictions = f
+    # YOUR CODE GOES HERE
+    avgy = np.mean(data)
+    top = (np.square(data - predictions)).sum()
+    bottom = (np.square(data - avgy)).sum()
+    r_squared = 1 - (top)/(bottom)
+    
+    
+    return r_squared
